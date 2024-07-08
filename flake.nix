c09ab39dd4abc9980c16d9d90d30d4a5c60daaf4
ThoFrank <thomas@franks-im-web.de>
2024-07-08 21:29:37 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Application packaged using poetry2nix";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    mealie = {
+      url = "github:mealie-recipes/mealie";
+      flake = false;
+    };
+    
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix, mealie }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      in
+      {
+        packages = {
+          myapp = mkPoetryApplication { projectDir = mealie; };
+          default = self.packages.${system}.myapp;
+        };
+      });
+}
